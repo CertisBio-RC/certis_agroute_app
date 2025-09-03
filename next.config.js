0a0382fb139ad64dbbc28b3908489eedcce930b7
Bailey <jbailey@certisbio.com>
2025-09-03 16:08:32 -0500

    fix: strip UTF-8 BOM so Next build passes on CI

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-﻿/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const repo = process.env.NEXT_PUBLIC_REPO_NAME || "certis_agroute_app";
 module.exports = {
   output: "export",
